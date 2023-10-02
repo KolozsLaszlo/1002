10064ab88adbbd99826b2f0f3c08c72240e9b072
kzs <Kolozsvari@a.a>
2023-10-02 21:05:48 +0200

    hf done

---
+++ script.js
@@ -0,0 +1,7 @@
+const adat1 = ["Kolozsvári László", "Diák", "Kisköre"]
+const adat2 = ["Példa Péter", "Tanár", "Kömlő"]
+const adat3 = ["Kitalált Jani", "Diák", "Eger"]
+const ossszetettadat = [adat1, adat2, adat3]
+for (let i=0; i<3;i++) {
+    console.log(ossszetettadat[i])
+}
